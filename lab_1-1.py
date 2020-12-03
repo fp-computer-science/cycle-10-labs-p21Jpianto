7af7c2a15647b844bbcf7cf0c91eca77674456a2
Joshua Pianto <p21jpianto@fairfieldprep.org>
2020-12-03 10:14:28 -0500

    Function Lab

---
+++ lab_1-1.py
@@ -0,0 +1,10 @@
+# Author: JAP (AMDG) 12/3/20
+
+# def greeting("Hello World!): "syntax error EOL"
+
+def greetings():
+    '''This prints Hello world'''
+    print("Hello World!")
+
+help(greetings)
+greetings()
---
+++ lab_1-2.py
@@ -0,0 +1,9 @@
+# Author: JAP (AMDG) 12/3/20
+
+def find_sum(num1, num2):
+    '''Add num1 to num2'''
+    num_sum = num1 + num2
+    return num_sum
+
+my_sum = find_sum(111,222)
+print(my_sum)
