8e966f807967dba1c2a177d37b58500d872106fe
Joshua Pianto <p21jpianto@fairfieldprep.org>
2020-12-07 13:15:34 -0500

    lab

---
+++ lab_3-1.py
@@ -0,0 +1,14 @@
+# Author:JAP 12/7/2020  (amdg)
+
+
+def sum_to(n):
+    toal = 0
+    for i in range(n + 1):
+        total += 1
+    return total
+
+
+num = input("Please give a number")
+
+results = sum_to(into(num))
+print(results)
